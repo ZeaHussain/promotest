82b60af213f2e3a81df8cda6cbc6d9da8a472a2d
ZeaHussain <yamyam.dalligos@gmail.com>
2022-09-11 15:03:36 +0300

    just text

--- lib/main.dart
+++ lib/main.dart
@@ -50,7 +50,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed this many times:',
+              'just text',
             ),
             Text(
               '$_counter',
